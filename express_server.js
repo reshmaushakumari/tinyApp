c239db24f6dd35198caebc2da2381f18258100fc
reshmaushakumari <reshma.ushakumari@outlook.com>
2020-06-08 18:33:56 +0000

    Adding routes

--- express_server.js
+++ express_server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
 app.get('/',(req,res) => {
   res.send("Hello!");
+})
+
+app.get('/urls.json',(req,res) => {
+  res.send(urlDatabase)
 })
 
 app.listen(PORT,() => {
