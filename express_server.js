fd7f5ea90fd3c41bddafb1ca1d2a1b9c72112496
reshmaushakumari <reshma.ushakumari@outlook.com>
2020-06-08 22:41:02 +0000

    Fill out urls_index template

--- express_server.js
+++ express_server.js
@@ -18,7 +18,14 @@ app.get('/urls.json',(req,res) => {
 })
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  let templateVars = {greeting : "Hello World"}
+  res.render("hello_world", templateVars);
+  //res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT,() => {
